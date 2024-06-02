02f5fbeb488c493d3b60dcb091519525c45588f3
ariessa <ariessa@users.noreply.github.com>
2024-06-03 05:57:31 +0800

    WIP: Debug function calculate_lp

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ app.post('/api/calculate_lp', async (req, res) => {
     try {
         const calc_result = await liquidity_pool.calculate_lp(lp_address, wallet_address);
 
+        // TODO: Debug function calculate_lp
+
+        console.log("calc: ", await calc_result);
+
         // Check if calc_result is a valid result
         if (calc_result === null || typeof calc_result === 'undefined') {
             return res.status(422).json({ error: 'Invalid result from calculate_lp function' });
