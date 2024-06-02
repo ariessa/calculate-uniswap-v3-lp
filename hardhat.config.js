75484ab21378377f819e95c28c1ac86182691e47
ariessa <ariessa@users.noreply.github.com>
2024-06-02 23:59:23 +0800

    Remove package hardhat-verify

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: `${process.env.INFURA_TESTNET_URL}${process.env.INFURA_API_KEY}`,
+      accounts: [`0x${process.env.WALLET_TESTNET_PRIVATE_KEY}`]
+    },
+  }
 };
