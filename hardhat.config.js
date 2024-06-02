07f8a316a387da475f168de535ec36d2d937d8af
ariessa <ariessa@users.noreply.github.com>
2024-06-03 02:24:53 +0800

    Update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,15 @@ require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
   networks: {
     sepolia: {
       url: `${process.env.INFURA_TESTNET_URL}${process.env.INFURA_API_KEY}`,
