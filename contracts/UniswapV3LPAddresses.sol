0474ba66c70cabf518b33fb7e9f39ecff3710098
ariessa <ariessa@users.noreply.github.com>
2024-06-02 23:57:53 +0800

    Add finalised smart contract and its abi

--- contracts/UniswapV3LPAddresses.sol
+++ contracts/UniswapV3LPAddresses.sol
@@ -6,53 +6,104 @@ import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 /**
  * @title UniswapV3LPAddresses
  * @dev This contract represents a list of Uniswap V3 liquidity pool contracts.
- *      It allows users to add and remove liquidity, as well as perform swaps.
+ *      It allows users to add and remove liquidity pool, as well as get the details of a liquidity pool.
  */
 contract UniswapV3LPAddresses is AccessControl {
     // Struct to represent liquidity pool
     struct Liquidity_Pool {
         // The address of the first token
-        address token0;
+        address token_0;
         // The address of the second token
-        address token1;
-        // The symbol of the first token
-        string token0_symbol;
-        // The symbol of the second token
-        string token1_symbol;
-        // The name of the first token
-        string token0_name;
-        // The name of the second token
-        string token1_name;
+        address token_1;
+        // The fee of the liquidity pool
+        uint24 fee;
+        // The flag to indicate whether the liquidity pool exists or not
+        bool exist;
     }
 
     // Mapping to store liquidity pools
-    mapping(address => Liquidity_Pool[]) public liquidity_pools;
+    mapping(address => Liquidity_Pool) public liquidity_pools;
 
-    // Create a new role identifier for the LP_MANAGER role
+    // Last processed block in mainnet with status finalised
+    uint256 private last_block;
+
+    // Role identifier for the LP_MANAGER role
     bytes32 public constant LP_MANAGER_ROLE = keccak256("LP_MANAGER_ROLE");
+
+    /// @notice Emitted when a pool is added
+    /// @param token_0 The first token of the pool by address sort order
+    /// @param token_1 The second token of the pool by address sort order
+    /// @param fee The fee collected upon every swap in the pool, denominated in hundredths of a bip
+    /// @param pool The address of the added pool
+    event PoolAdded(
+        address indexed token_0,
+        address indexed token_1,
+        uint24 indexed fee,
+        address pool
+    );
+
+    constructor(uint256 _last_block) {
+        last_block = _last_block;
+        _grantRole(LP_MANAGER_ROLE, msg.sender);
+    }
 
     /**
      * @dev Adds new liquidity pool.
      * @param lp_address The address of the liquidity pool.
-     * @param _token0 The address of the first token.
-     * @param _token1 The address of the second token.
-     * @param _token0_symbol The symbol of the first token.
-     * @param _token1_symbol The symbol of the second token.
-     * @param _token0_name The name of the first token.
-     * @param _token1_name The name of the second token.
+     * @param token_a The address of the first token.
+     * @param token_b The address of the second token.
+     * @param fee The fee of the liquidity pool, denominated in hundredths of a bip (1e-6).
+     * @param block_number The block number on mainnet when liquidity pool is created.
      */
     function add_liquidity_pool(
         address lp_address,
-        address _token0,
-        address _token1,
-        string memory _token0_symbol,
-        string memory _token1_symbol,
-        string memory _token0_name,
-        string memory _token1_name
+        address token_a,
+        address token_b,
+        uint24 fee,
+        uint256 block_number
     ) public onlyRole(LP_MANAGER_ROLE) {
-        liquidity_pools[lp_address].push(
-            Liquidity_Pool(_token0, _token1, _token0_symbol, _token1_symbol, _token0_name, _token1_name)
+        require(token_a != token_b, "Token addresses are the same!");
+        (address token_0, address token_1) = token_a < token_b ? (token_a, token_b) : (token_b, token_a);
+        require(token_0 != address(0), "First token address is a zero or null address!");
+        require(liquidity_pools[lp_address].exist == false, "Liquidity pool already exists!");
+        liquidity_pools[lp_address] = Liquidity_Pool(token_0, token_1, fee, true);
+        _set_last_block(block_number);
+        emit PoolAdded(token_0, token_1, fee, lp_address);
+    }
+
+    /**
+     * @dev Adds multiple liquidity pools.
+     * @param lp_address The addresses of the liquidity pool.
+     * @param token_a The addresses of the first token.
+     * @param token_b The addresses of the second token.
+     * @param fee The fee of liquidity pools, denominated in hundredths of a bip (1e-6).
+     * @param block_number The lowest block number from a batch of liquidity pools' event logs.
+     */
+    function batch_add_liquidity_pool(
+        address[] memory lp_address,
+        address[] memory token_a,
+        address[] memory token_b,
+        uint24[] memory fee,
+        uint256 block_number
+    ) public onlyRole(LP_MANAGER_ROLE) {
+        require(
+            lp_address.length == token_a.length &&
+            lp_address.length == token_b.length &&
+            lp_address.length == fee.length,
+            "Array lengths mismatch!"
         );
+
+        for (uint256 i = 0; i < lp_address.length; i++) {
+            if(token_a[i] != token_b[i]) {
+                    (address token_0, address token_1) = token_a[i] < token_b[i] ? (token_a[i], token_b[i]) : (token_b[i], token_a[i]);
+
+                    if (token_0 != address(0) && liquidity_pools[lp_address[i]].exist == false) {
+                        liquidity_pools[lp_address[i]] = Liquidity_Pool(token_0, token_1, fee[i], true);
+                        _set_last_block(block_number);
+                        emit PoolAdded(token_0, token_1, fee[i], lp_address[i]);
+                    }
+            }
+        }
     }
 
     /**
@@ -68,11 +119,30 @@ contract UniswapV3LPAddresses is AccessControl {
     /**
      * @dev Gets the details of a liquidity pool.
      * @param lp_address The address of the liquidity pool.
-     * @return The details of a liquidity pool.
+     * @return Liquidity_Pool The details of a liquidity pool.
      */
     function get_liquidity_pool(
         address lp_address
-    ) public view returns (Liquidity_Pool[] memory) {
+    ) public view returns (Liquidity_Pool memory) {
+        require(liquidity_pools[lp_address].exist == true, "Liquidity pool does not exist!");
         return liquidity_pools[lp_address];
+    }
+
+    /**
+     * @dev Sets the value of last processed block in mainnet with status finalised.
+     * @param block_number The new block number in mainnet with status finalised.
+     */
+    function _set_last_block(uint256 block_number) internal onlyRole(LP_MANAGER_ROLE) {
+        require(block_number != 0, "New block number cannot be zero!");
+        require(block_number > last_block, "New block number must be bigger than the old block number!");
+        last_block = block_number;
+    }
+
+    /**
+     * @dev Gets the value of last processed block in mainnet with status finalised.
+     * @return uint256 The value of last processed block in mainnet with status finalised.
+     */
+    function get_last_block() public view returns (uint256) {
+        return last_block;
     }
 }
