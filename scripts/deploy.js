47c605f85d5df06302168b6169a993fdb391e3e3
ariessa <ariessa@users.noreply.github.com>
2024-06-04 05:19:39 +0800

    Clean and refactor code

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,17 @@
 const hre = require("hardhat");
 const utils = require("../lib/utils");
+const constants = require("../lib/constants");
 
 async function main() {
-    const last_block = await utils.get_contract_creation_block();
+    const last_block = await utils.get_contract_creation_block(constants.UNISWAP_V3_FACTORY_ADDRESS);
     const UniswapV3LPAddresses = await hre.ethers.getContractFactory("UniswapV3LPAddresses");
     const contract = await UniswapV3LPAddresses.deploy(last_block);
 
     // Update value in .env file
-    utils.update_contract_address('UNISWAP_V3_LP_ADDRESSES_ADDRESS=', `UNISWAP_V3_LP_ADDRESSES_ADDRESS="${contract.target}"`);
+    utils.update_contract_address("../.env", 'UNISWAP_V3_POOL_ADDRESSES_ADDRESS=', `UNISWAP_V3_POOL_ADDRESSES_ADDRESS="${contract.target}"`);
 
-    // Create abi file for contract UniswapV3LPAddresses
-    utils.create_abi();
+    // Create abi file for contract UniswapV3PoolAddresses
+    utils.create_json_file("artifacts/contracts/UniswapV3LPAddresses.sol/UniswapV3LPAddresses.json", "UniswapV3LPAddresses.json", "abi/UniswapV3LPAddresses.json");
 }
 
 main()
